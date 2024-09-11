89b9e0cce8a23f3de2137457efbbb2638b03cc2b
bartelsj-code <jonasbartels8@gmail.com>
2024-09-11 15:48:28 -0700

    url uses id

--- flask_interface.py
+++ flask_interface.py
@@ -1,7 +1,34 @@
+import sqlite3
 from flask import Flask, render_template
+from werkzeug.exceptions import abort
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def get_movie(movie_id):
+    conn = get_db_connection()
+    movie = conn.execute('SELECT * FROM movies WHERE id = ?',
+                        (movie_id,)).fetchone()
+    conn.close()
+    if movie is None:
+        abort(404)
+    return movie
+
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    conn = get_db_connection()
+    movies = conn.execute('SELECT * FROM movies').fetchall()
+    conn.close()
+    return render_template('index.html', movies=movies)
+
+@app.route('/<int:movie_id>')
+def movie(movie_id):
+    movie = get_movie(movie_id)
+    return render_template('movie.html', movie = movie)
+
--- schema.sql
+++ schema.sql
@@ -3,5 +3,11 @@ DROP TABLE IF EXISTS movies;
 CREATE TABLE movies (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     title TEXT NOT NULL,
-    year YEAR    
+    year Year,
+    watched BOOL DEFAULT FALSE,
+    watchlist BOOL DEFAULT FALSE,
+    priority TINYINT DEFAULT 0,
+    rating TINYINT,
+    comment VARCHAR(1000),
+    lb_link VARCHAR(200)
 );
